173b311277677839cd7ee6f46836cccb7089bde2
igl-myrick <igl.myrick@gmail.com>
2024-09-17 15:11:52 -0700

    update requirements type and add GetAll and ClearAll methods

--- JobBoard/Models/Listing.cs
+++ JobBoard/Models/Listing.cs
@@ -6,7 +6,27 @@ namespace JobBoard.Models
   {
     public string Title { get; set; }
     public string Description { get; set; }
-    public List<string> Requirements { get; set; }
+    public string Requirements { get; set; }
     public string ContactInfo { get; set; }
+
+    private static List<Listing> _instances = new List<Listing> { };
+
+    public Listing(string title, string description, string requirements, string contactInfo) {
+      Title = title;
+      Description = description;
+      Requirements = requirements;
+      ContactInfo = contactInfo;
+      _instances.Add(this);
+    }
+    
+    public static List<Listing> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
